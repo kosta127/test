93062a5b02b282d563ee8eb5fdfffa3ca7de142e
kosta <bbbyo.o@gmail.com>
2016-10-10 15:43:29 +0900

    새 액셔 만듦 ㅎㅎ
    Signed-off-by: kosta <bbbyo.o@gmail.com>

---
+++ gitTest/src/kosta/action/ForGitTestNewAction.java
@@ -0,0 +1,15 @@
+package kosta.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ForGitTestNewAction implements Action{
+
+	@Override
+	public ActionForward execute(HttpServletRequest request,
+			HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+ 
+}
