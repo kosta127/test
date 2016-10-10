9356ff9ad325402591e025d0350258f247530ad4
kosta <bbbyo.o@gmail.com>
2016-10-10 15:47:54 +0900

    my own action

---
+++ gitTest/src/kosta/action/OwnAction.java
@@ -0,0 +1,15 @@
+package kosta.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OwnAction implements Action{
+
+	@Override
+	public ActionForward execute(HttpServletRequest request,
+			HttpServletResponse response) {
+		// OWN Action 
+		return null;
+	}
+	
+}
